ce090918302897dabe3eacfdf552501b48b1c816
c0dem0de <codemode127@gmail.com>
2021-10-18 14:03:52 +0530

    Login File added

--- bin.py
+++ bin.py
@@ -106,33 +106,33 @@
 #-----------------------------------------------------------------------------------------------------------------------------------------------------#
 #-----------------------------------------------------------------------------------------------------------------------------------------------------#
 
-import mysql.connector
-from bullet import Password
+# import mysql.connector
+# from bullet import Password
 
-def Login():
-	userName = input("Enter user name: ")
-	password = Password(prompt='Enter Password: ', hidden='*').launch()
+# def Login():
+# 	userName = input("Enter user name: ")
+# 	password = Password(prompt='Enter Password: ', hidden='*').launch()
 
-	connection = mysql.connector.connect(host='localhost', user=f'{userName}', passwd=f'{password}')
+# 	connection = mysql.connector.connect(host='localhost', user=f'{userName}', passwd=f'{password}')
 	
-	main_cursor = connection.cursor()
+# 	main_cursor = connection.cursor()
 
-	databases = []
+# 	databases = []
 
-	main_cursor.execute("SHOW DATABASES")
+# 	main_cursor.execute("SHOW DATABASES")
 
-	for x in main_cursor:
-	  databases.append(x[0])
+# 	for x in main_cursor:
+# 	  databases.append(x[0])
 
 
-	if 'bms' in databases:
-		bms_connect = mysql.connector.connect(host='localhost', user=f'{userName}', passwd=f'{password}', database='bms') 
-		print("Connection Made!!")
+# 	if 'bms' in databases:
+# 		bms_connect = mysql.connector.connect(host='localhost', user=f'{userName}', passwd=f'{password}', database='bms') 
+# 		print("Connection Made!!")
 	
-	else:
-		main_cursor.execute("CREATE DATABASE bms")
-		print("Database Created!!")
-		bms_connect = mysql.connector.connect(host='localhost', user=f'{userName}', passwd=f'{password}', database='bms')
-		print("Connection Made!!")	
+# 	else:
+# 		main_cursor.execute("CREATE DATABASE bms")
+# 		print("Database Created!!")
+# 		bms_connect = mysql.connector.connect(host='localhost', user=f'{userName}', passwd=f'{password}', database='bms')
+# 		print("Connection Made!!")	
 
-Login()
+# Login()
