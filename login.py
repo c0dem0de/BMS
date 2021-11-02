6e86575a3ffcec181ea5cfc70bd3ee63b8168a24
chaitanyakelkar <chaitanyaakelkar57@gmail.com>
2021-11-02 20:38:22 +0530

    coded create account others remaining

--- login.py
+++ login.py
@@ -1,14 +1,15 @@
 from bullet import Password
-from encrypter import encrypter
+# from encrypter import encrypter
 import mysql.connector
 from os import system as sys
+import employee_menu
 # from modulez import *
 
 
-# usr = input("Enter usrname: ")
-# pwd = input("Enter pwd: ")
-usr = "home"
-pwd = "Trio1-Pope-Overbill"
+usr = input("Enter usrname: ")
+pwd = Password(prompt='Enter pwd: ', hidden='*').launch()
+# usr = "home"
+# pwd = "Trio1-Pope-Overbill"
 base = mysql.connector.connect(host='localhost', user=f'{usr}', passwd=f'{pwd}', database='bms')
 pointr = base.cursor()
 sys('clear')
@@ -42,18 +43,21 @@ def Login():
 	# check credentials
 	if userName in employees:
 		password = Password(prompt='\t ⇢ Password: ', hidden='*').launch()
-		encrypted_Key = encrypter(password)
+		encrypted_Key = password
 
 		if encrypted_Key in passwords:
 			print("\t    Logged In ✔")
 			print("\t  Hello Employee")
+			input("Press Enter to continue")
+			sys('clear')
+			employee_menu.main(base,pointr)
 
 		else:
 			print("      !!Incorrect Password!!")
 
 	elif userName in members:
 		password = Password(prompt='\t ⇢ Password: ', hidden='*').launch()
-		encrypted_Key = encrypter(password)
+		encrypted_Key = password
 
 		if encrypted_Key in mem_passwords:
 			print("\t    Logged In ✔")
